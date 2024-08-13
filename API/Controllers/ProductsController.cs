d9a1fa16ce5babb5e9a11b14590ebfef6ad9cfca
piyush832 <piyushnm13@gmail.com>
2024-08-13 21:55:17 +0530

    DB and Entityframework creation and link

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext context;
+
+        //Constructor
+        public ProductsController(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await context.products.ToListAsync();
+            return products;
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductsById(int id)
+        {
+            return await context.products.FindAsync(id);
+        }
+    }
+}
